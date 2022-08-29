31dca2f6ee82654f87e024cdbccf2298228001c7
bennie-asi <1820313040@qq.com>
2022-08-29 17:40:50 +0800

    让Markdown文章自动生成目录，提升阅读体验

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
+import re
+
 import markdown
 from django.shortcuts import render, get_object_or_404
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
 from .models import Post
 # Create your views here.
 from django.http import HttpResponse
@@ -16,11 +20,16 @@ def index(request):
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     # 添加Markdown语法拓展，包含额外拓展，语法高亮和自动生成目录
-    post.body = markdown.markdown(post.body, extensions=[
+    md = markdown.Markdown(extensions=[
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
         # 'markdown.extensions.fenced_code',
-        'markdown.extensions.toc',
+        # 'markdown.extensions.toc',
+        TocExtension(slugify=slugify),
     ])
-
+    post.body = md.convert(post.body)
+    #  post 实例本身是没有 toc 属性的，给它动态添加了 toc 属性
+    # post.toc = md.toc
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
     return render(request, 'blog/detail.html', context={'post': post})
