1b03f4e832e341294e855899330ba7d0cf554196
bennie-asi <1820313040@qq.com>
2022-08-30 16:20:56 +0800

    评论功能的实现

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'  #注册blog应用
+    'blog.apps.BlogConfig',  #注册blog应用
+    'comments.apps.CommentsConfig', #注册comments应用
 ]
 
 MIDDLEWARE = [
